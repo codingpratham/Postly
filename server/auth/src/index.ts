cc84a3273ce5cf3633a3161f212c5d8a412dfa6e
codingpratham <coding.job17@gmail.com>
2026-07-13 17:32:41 +0530

    .

--- server/auth/src/index.ts
+++ server/auth/src/index.ts
@@ -1,9 +1,12 @@
 import "dotenv/config";
 import express from "express";
-
+import authRouter from "./routes/auth.r.js";
 const app = express();
 
 const PORT = process.env.PORT1 || 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api',authRouter);
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello, World!");
