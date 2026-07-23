62eb23256f1a0d95b0d0846b25d4b8533debdfc4
codingpratham <coding.job17@gmail.com>
2026-07-23 23:08:40 +0530

    setting up the foundation of services

--- server/post/src/middleware/auth.m.ts
+++ server/post/src/middleware/auth.m.ts
@@ -1,5 +1,35 @@
 import type {Request,Response,NextFunction} from "express"
+import jwt from "jsonwebtoken"
 
-export const AuthMiddleware = async (req:Request , res:Response, next:NextFunction)=>{
+declare global {
+    namespace Express {
+        interface Request {
+            userId?: string;
+        }
+    }
+}
 
+export const authenticate = async(req:Request , res:Response, next:NextFunction)=>{
+    const token = req.cookies.accessToken;
+
+    if(!token){
+        res.status(401).json({message: "Unauthorized"});
+    }
+
+    try{
+        const decoded = await jwt.verify(
+            token,
+            process.env.ACCESS_TOKEN_SECRET as string
+        ) as { userId: string }
+
+        req.userId = decoded.userId;
+        
+        next(); 
+    }
+    catch(error){
+        console.log("Error in authentication",error)
+        res.status(404).json({
+            "message":"internal server error"
+        })
+    }
 }
